ad242c21b7c0da71603988742e24b847551fdedc
Joshua Lokken <jrrlokken@gmail.com>
2020-11-14 12:15:15 -0600

    Render navbar, home in App.  Apply basic styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Home />
+      </BrowserRouter> 
+    </div>
   );
 }
 
