8daa58d603dce4107e530c59a84ce5ff34a8a735
Joshua Lokken <jrrlokken@gmail.com>
2020-11-14 12:17:42 -0600

    Add navbar, styles

---
+++ src/NavBar.js
@@ -0,0 +1,29 @@
+import { Navbar, Nav, NavItem } from "reactstrap";
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div>
+      <Navbar expand="xs">
+        <NavLink exact to="/" className="navbar-brand">
+          Jobly
+        </NavLink>
+
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink to="">Companies</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="">Jobs</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="">Profile</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
