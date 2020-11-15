c72065296b2d387433f175c336005d8216faa71e
Joshua Lokken <jrrlokken@gmail.com>
2020-11-15 12:04:36 -0600

    Added local storage hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+const useLocalStorage = (key, defaultValue = null) => {
+  const initialValue = localStorage.getItem(key) || defaultValue;
+  const [item, setItem] = useState(initialValue);
+
+  useEffect(() => {
+    if (!item) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, item);
+    }
+  }, [key, item]);
+
+  return [item, setItem];
+}
+
+export default useLocalStorage;
