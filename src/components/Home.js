d54bc1428674707b16d826997eddc36cfcba6f6d
Joshua Lokken <jrrlokken@gmail.com>
2020-11-17 14:14:09 -0600

    Added conditional welcome text for logged in user

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
-import { Container, Button } from 'reactstrap';
+import { useContext } from 'react';
+import { Container } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import UserContext from '../UserContext';
 import '../styles/Home.css';
 
 const Home = () => {
+  const { currentUser } = useContext(UserContext);
   return(
     <Container className="Home">
       <h1 className="mb-4 font-weight-bold">Jobly</h1>
-      <p class="lead">All the jobs in one convenient place.</p>
-      <Button color="primary font-weight-bold">Log In</Button>
+      <p className="lead">All the jobs in one convenient place.</p>
+      {currentUser ? (
+        <h2>Welcome Back {currentUser.first_name}!</h2>
+      ) : (
+        <Link className="btn btn-primary font-weight-bold" to="/login">Log In</Link>
+      )}  
     </Container>
   )
 }
