4565d3f8c15b62dce192dc9b40585d3fe40bb8ef
Joshua Lokken <jrrlokken@gmail.com>
2020-11-17 14:15:01 -0600

    Finish building out search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,32 @@
-const Search = () => {
+import { useState } from 'react';
+import { Container, Form, Input, Button } from 'reactstrap';
+import '../styles/Search.css';
+
+const Search = ({ searchFor }) => {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    searchFor(search);
+  }
+
+  const handleChange = evt => {
+    setSearch(evt.target.value);
+  }
+
   return (
-    <div>
-      Search
-    </div>
+    <Container className="Search mb-4">
+      <Form className="form-inline" onSubmit={handleSubmit}>
+        <Input
+          className="form-control form-control-lg flex-grow-1"
+          name="search"
+          placeholder="Enter search term..."
+          value={search}
+          onChange={handleChange}
+        />
+        <Button size="lg" color="primary">Submit</Button>
+      </Form>
+    </Container>
   )
 }
 
