fa80de85523433e07d983589785c475376876892
Joshua Lokken <jrrlokken@gmail.com>
2020-11-17 14:14:33 -0600

    Added logged in / logged out nav menus

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,51 @@
+import { useContext } from 'react';
 import { Navbar, Nav, NavItem } from "reactstrap";
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import '../styles/NavBar.css';
+import UserContext from '../UserContext';
 
-const NavBar = () => {
-  return (
-    <div>
-      <Navbar expand="xs">
-        <NavLink exact to="/" className="navbar-brand">
-          Jobly
-        </NavLink>
+const NavBar = ({ logout }) => {
+  const {currentUser} = useContext(UserContext);
 
-        <Nav className="ml-auto" navbar>
-          <NavItem>
+  const loggedInNav = () => {
+    return (
+        <>
+          <NavItem className="mr-4">
             <NavLink to="/companies">Companies</NavLink>
           </NavItem>
-          <NavItem>
+          <NavItem className="mr-4">
             <NavLink to="/jobs">Jobs</NavLink>
           </NavItem>
-          <NavItem>
+          <NavItem className="mr-4">
             <NavLink to="/profile">Profile</NavLink>
           </NavItem>
-        </Nav>
-      </Navbar>
-    </div>
+          <NavLink className="nav-link" to="/" onClick={logout}>Log out</NavLink>
+        </>
+        
+        
+    );
+  }
+
+  const loggedOutNav = () => {
+    return (
+      <>
+        <NavItem className="mr-4">
+          <NavLink to="/login">
+            Login
+          </NavLink>
+        </NavItem>
+      </> 
+    );
+  }
+
+  return (
+    <Navbar className="mb-4">
+      <NavLink exact to="/" className="navbar-brand">
+        Jobly
+      </NavLink>
+      {loggedInNav()}
+      {/* {currentUser ? loggedInNav() : loggedOutNav()} */}
+    </Navbar>
   );
 }
 
