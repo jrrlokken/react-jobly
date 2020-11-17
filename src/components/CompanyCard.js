af8b27392663d2372c101496612f7aa73f5f7853
Joshua Lokken <jrrlokken@gmail.com>
2020-11-17 14:13:39 -0600

    Finish building out CompanyCard component

--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -1,23 +1,30 @@
 import {
-  Button,
   Card,
   CardBody,
   CardTitle,
-  CardText
+  CardText,
+  CardImg
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import defaultLogo from '../default_logo.png';
+import '../styles/CompanyCard.css';
 
-const CompanyCard = () => {
+const CompanyCard = ({ item = {} }) => {
+  const { name, description, logo_url, handle } = item;
   return (
-    <Card>
-      <CardBody>
-        <CardTitle>
-          Company Name
-        </CardTitle>
-        <CardText>
-          Company Text
-        </CardText>
-      </CardBody>
-    </Card>
+    <Link to={`/companies/${handle}`}>
+      <Card>
+        <CardBody>
+          <CardTitle className="d-inline-block">
+            Company Name
+          </CardTitle>
+          <CardImg className="CompanyCard-logo d-inline-block" src={logo_url || defaultLogo} />
+          <CardText className="d-inline-block">
+            Company Description
+          </CardText>
+        </CardBody>
+      </Card>
+    </Link>
   )
 }
 
