ced4bb0eeddafcf7e5e087ecbb63e69415209cee
Joshua Lokken <jrrlokken@gmail.com>
2020-11-17 14:12:23 -0600

    Finished building out companies component

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,13 +1,29 @@
+import { useState, useEffect } from 'react';
 import { Container } from 'reactstrap';
-import CompanyCard from './CompanyCard';
+import CardList from './CardList';
+import Search from './Search';
+import JoblyAPI from '../JoblyAPI';
 
 const Companies = () => {
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    async function getCompanies() {
+      let companies = await JoblyAPI.getCompanies();
+      setCompanies(companies);
+    }
+    getCompanies();
+  }, []);
+
+  async function handleSearch(search) {
+    let companies = await JoblyAPI.getCompanies(search);
+    setCompanies(companies);
+  }
+
   return (
     <Container>
-      Companies
-      <div>
-        <CompanyCard />
-      </div>
+      <Search endpoint="companies" searchFor={handleSearch} />
+      <CardList cards={companies} />
     </Container>
   )
 }
