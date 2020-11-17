d86bec1d892eee0cac13cd4ca5628a5c75526f21
Joshua Lokken <jrrlokken@gmail.com>
2020-11-17 14:12:47 -0600

    Finish building out jobs component

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,13 +1,31 @@
-import JobCard from './JobCard';
+import { useState, useEffect } from 'react';
+import CardList from './CardList';
+import Search from './Search';
+import JoblyAPI from '../JoblyAPI';
 import { Container } from 'reactstrap';
 
 const Jobs = () => {
+  const [jobs, setJobs] = useState([]);
+
+  async function search(search) {
+    let jobs = await JoblyAPI.getJobs(search);
+    setJobs(jobs);
+  }
+
+  useEffect(() => {
+    search();
+  }, []);
+
+  async function apply(idx) {
+    let jobId = jobs[idx].id;
+    let message = await JoblyAPI.applyToJob(jobId);
+    setJobs(j => j.map(job => job.id === jobId ? { ...job, state: message} : job));
+  }
+
   return (
-    <Container>
-      Jobs
-      <div>
-        <JobCard />
-      </div>
+    <Container className="Jobs">
+      <Search endpoint="jobs" searchFor={search} />
+      <CardList cards={jobs} apply={apply} />
     </Container>
   )
 }
