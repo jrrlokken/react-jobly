877b5754b8378e15aad1a6a19dc4df971865c629
Joshua Lokken <jrrlokken@gmail.com>
2020-11-18 13:30:32 -0600

    Implement job card logic for ui

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -6,21 +6,48 @@ import {
   CardText
 } from 'reactstrap';
 
-const JobCard = () => {
+const JobCard = ({ item = {}, handleApply }) => {
+  const { title, salary, equity } = item;
+
+  // return (
+  //   <Card>
+  //     <CardBody>
+  //       <CardTitle className="d-flex justify-content-between">
+  //         <span className="text-capitalize">{title}</span>
+  //       </CardTitle>
+  //       <CardText>
+  //         <div>Salary: {salary}</div>
+  //         <div>Equity: {equity}</div>
+  //       </CardText>
+  //       <Button 
+  //         color="primary"
+  //         className="font-weight-bold text-uppercase float-right"
+  //         onClick={handleApply}
+  //         disabled={item.state}  
+  //       >
+  //         {item.state ? "Applied" : "Apply"}
+  //       </Button>
+  //     </CardBody>
+  //   </Card>
+  // )
+
   return (
-    <Card>
-      <CardBody>
-        <CardTitle>
-          Job Title
-        </CardTitle>
-        <CardText>
-          Job Description
-        </CardText>
-        <Button color="primary">
-          Apply
-        </Button>
-      </CardBody>
-    </Card>
+    <div className="Card card">
+      <div className="card-body">
+        <h6 className="card-title d-flex justify-content-between">
+          <span className="text-capitalize">{title}</span>
+        </h6>
+        <div>Salary: {salary}</div>
+        <div>Equity: {equity}</div>
+        <button
+          className="btn btn-danger font-weight-bold text-uppercase float-right"
+          onClick={handleApply}
+          disabled={item.state}
+        >
+          {item.state ? "Applied" : "Apply"}
+        </button>
+      </div>
+    </div>
   )
 }
 
