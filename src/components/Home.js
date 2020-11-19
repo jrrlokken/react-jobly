c7f52b56e894f3e5989ace6d2b332576254d9fcb
Joshua Lokken <jrrlokken@gmail.com>
2020-11-19 11:23:14 -0600

    Removed container from Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,17 +6,20 @@ import '../styles/Home.css';
 
 const Home = () => {
   const { currentUser } = useContext(UserContext);
+
   return(
-    <Container className="Home">
-      <h1 className="mb-4 font-weight-bold">Jobly</h1>
-      <p className="lead">All the jobs in one convenient place.</p>
-      {currentUser ? (
-        <h2>Welcome Back {currentUser.first_name}!</h2>
-      ) : (
-        <Link className="btn btn-primary font-weight-bold" to="/login">Log In</Link>
-      )}  
-    </Container>
-  )
+    <div className="Home">
+      <div className="container text-center">
+        <h1 className="mb-4 font-weight-bold">Jobly</h1>
+        <p className="lead">All the jobs in one convenient place.</p>
+        {currentUser ? (
+          <h2>Welcome Back {currentUser.first_name}!</h2>
+        ) : (
+          <Link className="btn btn-primary font-weight-bold" to="/login">Log In</Link>
+        )}  
+      </div>
+    </div>
+  );
 }
 
 export default Home;
