bccc4fa12b81d76fcbd514d4941fbe7eb5f959f7
Joshua Lokken <jrrlokken@gmail.com>
2020-11-20 12:48:27 -0600

    Fixed no data returned issue

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,14 +3,14 @@ import Card from './Card';
 function CardList({ cards = [], apply = () => null }) {
   return cards.length ? (
     <div className="CardList">
-      {cards.map((cardData, idx) => {
+      {cards.map((cardData, idx) => (
         <Card
           item={cardData}
           key={idx}
           idx={idx}
           apply={apply}
         />
-      })}
+      ))}
     </div>
   ) : (
     <p className="lead">Sorry, no results found</p>
