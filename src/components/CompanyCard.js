69c327d40d820d91680503dbb61d8a09dfdcb88d
Joshua Lokken <jrrlokken@gmail.com>
2020-11-20 12:49:45 -0600

    Moved to "generic" bootstrap classes from reactstrap

--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -1,37 +1,22 @@
-// import {
-//   Card,
-//   CardBody,
-//   CardTitle,
-//   CardText,
-//   CardImg
-// } from 'reactstrap';
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardImg
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import defaultLogo from '../default_logo.png';
 
 const CompanyCard = ({ item = {} }) => {
   const { name, description, logo_url, handle } = item;
-  // return (
-  //   <Link to={`/companies/${handle}`}>
-  //     <Card>
-  //       <CardBody>
-  //         <CardTitle className="d-flex justify-content-between">
-  //           <span className="text-capitalize">{name}</span>
-  //         </CardTitle>
-  //         <CardImg className="CompanyCard-logo d-inline-block" src={logo_url || defaultLogo} />
-  //         <CardText>
-  //           {description}
-  //         </CardText>
-  //       </CardBody>
-  //     </Card>
-  //   </Link>
-  // )
 
   return (
     <Link className="Card card" to={`/companies/${handle}`}>
       <div className="card-body">
         <h6 className="card-title d-flex justify-content-between">
           <span className="text-capitalize">{name}</span>
-          <img src={logo_url || defaultLogo} alt={`${name} Logo`} />
+          <img src={defaultLogo} alt={`${name} Logo`} />
         </h6>
         <p>{description}</p>
       </div>
