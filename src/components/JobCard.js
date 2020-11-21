41ecdc242c9fa7e6f81d8c80c1b92fd41f9bd265
Joshua Lokken <jrrlokken@gmail.com>
2020-11-21 07:16:30 -0600

    Add reactstrap elements

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,53 +1,30 @@
-// import {
-//   Button,
-//   Card,
-//   CardBody,
-//   CardTitle,
-//   CardText
-// } from 'reactstrap';
+import {
+  Button,
+  Card,
+  CardBody,
+} from 'reactstrap';
 
 function JobCard({ item = {}, handleApply }) {
   const { title, salary, equity } = item;
 
-  // return (
-  //   <Card>
-  //     <CardBody>
-  //       <CardTitle className="d-flex justify-content-between">
-  //         <span className="text-capitalize">{title}</span>
-  //       </CardTitle>
-  //       <CardText>
-  //         <div>Salary: {salary}</div>
-  //         <div>Equity: {equity}</div>
-  //       </CardText>
-  //       <Button 
-  //         color="primary"
-  //         className="font-weight-bold text-uppercase float-right"
-  //         onClick={handleApply}
-  //         disabled={item.state}  
-  //       >
-  //         {item.state ? "Applied" : "Apply"}
-  //       </Button>
-  //     </CardBody>
-  //   </Card>
-  // )
-
   return (
-    <div className="Card card">
-      <div className="card-body">
+    <Card className="Card">
+      <CardBody>
         <h6 className="card-title d-flex justify-content-between">
           <span className="text-capitalize">{title}</span>
         </h6>
         <div>Salary: {salary}</div>
         <div>Equity: {equity}</div>
-        <button
-          className="btn btn-danger font-weight-bold text-uppercase float-right"
+        <Button
+          color="danger"
+          className="font-weight-bold text-uppercase float-right"
           onClick={handleApply}
           disabled={item.state}
         >
           {item.state ? "Applied" : "Apply"}
-        </button>
-      </div>
-    </div>
+        </Button>
+      </CardBody>
+    </Card>
   )
 }
 
